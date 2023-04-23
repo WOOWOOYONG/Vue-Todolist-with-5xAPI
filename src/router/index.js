f92207a2ba28a1c2f78dba8460e92e1ec14209a2
woowooyong <woowooyong@gmail.com>
2023-04-23 20:01:11 +0800

    fix: 修正router路徑

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import TodoView from '../views/TodoView.vue';
 
 const routes = [
   {
@@ -7,18 +8,15 @@ const routes = [
     name: 'home',
     component: HomeView,
     meta: {
-      title: '首頁',
+      title: 'MyTodoList-首頁',
     },
   },
   {
     path: '/todo',
     name: 'todo',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/TodoView.vue'),
+    component: TodoView,
     meta: {
-      title: '待辦事項',
+      title: 'MyTodoList-待辦事項',
       requiresAuth: true,
     },
   },
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/Vue-Todolist-with-5xAPI/',
 });
